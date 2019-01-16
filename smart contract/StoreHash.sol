eb971ed2f4c2bd9460c2d53b79aacc9987a1f08d
Zarif Ikhmal <31915185+mzrfikhmal@users.noreply.github.com>
2019-01-17 04:43:24 +0800

    Update StoreHash.sol

--- smart contract/StoreHash.sol
+++ smart contract/StoreHash.sol
@@ -1,1 +1,36 @@
+pragma solidity ^0.4.24;
 
+contract Contract {
+ struct File {
+ string ipfsHash;
+ string fo;
+ string foo;
+ }
+ 
+ mapping (address => File) files;
+
+event hashInfo(
+        string ipfsHash     
+    );
+ 
+ function sendHash(address _address,string _ipfsHash) public {
+     var file = files[_address];
+   file.ipfsHash = _ipfsHash;
+   
+   emit hashInfo(_ipfsHash);
+ }
+ 
+function getHash(address _address) view public returns (string) {
+   return (files[_address].ipfsHash);
+ }
+ 
+ mapping (address => address) recipient;
+ 
+ function sendMessage(string msg1, string msg2, address _recipient) public {
+    files[_recipient].fo = msg1;
+    files[_recipient].foo = msg2;
+  }
+  function readMessage(address _address) public view returns (string , string) {
+    return (files[_address].fo, files[_address].foo);
+  }
+}
